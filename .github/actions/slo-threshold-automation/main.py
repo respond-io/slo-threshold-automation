de55606ab1f2bf795dfc24dce3d9a53560a5bfee
Haqim Azhar <luqman.h@rocketbots.io>
2024-11-28 15:44:29 +0800

    fix: remove unused function to list CloudWatch log groups from SLO threshold automation

--- .github/actions/slo-threshold-automation/main.py
+++ .github/actions/slo-threshold-automation/main.py
@@ -8,15 +8,6 @@ from utils.functions import (
 )
 from utils.cli import setup_cli
 import json
-import boto3
-
-
-def list_cloudwatch_log_groups():
-    client = boto3.client('logs', region_name='ap-southeast-1')
-    response = client.describe_log_groups()
-    log_groups = response.get('logGroups', [])
-    print(log_groups)
-    return [log_group['logGroupName'] for log_group in log_groups]
     
 def main():
     # Get the argument parser from cli.py and parse arguments
@@ -35,4 +26,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    list_cloudwatch_log_groups()
